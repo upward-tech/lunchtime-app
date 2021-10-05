c59fb1cdf097ad34227c02a8f7e5391b46d1d632
Adam Gaskins <adam@theadamgaskins.com>
2021-10-05 12:02:43 -0400

    Prevent node scripts from exiting & run silently

--- 1-install-node.bat
+++ 1-install-node.bat
@@ -2,5 +2,6 @@
 @echo Installing Node.js. This will take a few minutes...
 start /wait msiexec.exe /i https://nodejs.org/dist/v14.18.0/node-v14.18.0-x86.msi /passive
 @echo Setting up Lunchtime App...
-npm run setup
-npm run svc-install
+call npm install --silent
+call node setup.js
+call npm run svc-install
